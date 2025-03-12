457a394f160c642591fab73c6f7e87035be08fdc
Aleksey Kolobov <www.kolabs@gmail.com>
2025-03-12 18:39:45 +0300

    Улучшения

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -23,10 +23,12 @@ public class BookItemRequestDto {
     @NotNull(message = "Item ID cannot be null")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     long itemId;
+
     @NotNull(message = "Start time cannot be null")
     @FutureOrPresent(message = "Start time must be in the present or future")
     @JsonProperty("start")
     LocalDateTime start;
+
     @Future(message = "End time must be in the future")
     @NotNull(message = "End time cannot be null")
     @JsonProperty("end")
