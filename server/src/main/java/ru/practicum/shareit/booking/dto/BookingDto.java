231a7ebd5a77d808ad1d4c4a6b40676ad7e7986c
Aleksey Kolobov <www.kolabs@gmail.com>
2025-03-12 18:44:41 +0300

    Улучшения

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.booking.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +11,6 @@ import lombok.experimental.FieldDefaults;
 import ru.practicum.shareit.booking.BookingStatus;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.dto.UserDto;
-
 import java.time.LocalDateTime;
 
 @Builder
@@ -21,10 +22,16 @@ public class BookingDto {
 
     Long id;
 
+    @NotNull(message = "Start time cannot be null")
+    @JsonProperty("start")
     LocalDateTime startTime;
 
+    @NotNull(message = "End time cannot be null")
+    @JsonProperty("end")
     LocalDateTime endTime;
 
+    @NotNull(message = "Item ID cannot be null")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     Long itemId;
 
     BookingStatus status;
