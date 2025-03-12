738f5db690ba788bc1ca9abf5fe11e938f5621c3
Aleksey Kolobov <www.kolabs@gmail.com>
2025-03-12 21:13:44 +0300

    вернул валидацию в BookingDto сервера

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookItemRequestDto.java
@@ -1,11 +1,7 @@
 package ru.practicum.shareit.booking.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
