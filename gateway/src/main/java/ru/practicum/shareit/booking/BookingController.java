f0cf95b15b5d3fd7e943124da58d554ce0bf0f48
Aleksey Kolobov <www.kolabs@gmail.com>
2025-03-12 21:41:45 +0300

    вернул валидацию в BookingDto сервера

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingStatus;
 
